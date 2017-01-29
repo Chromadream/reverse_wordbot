0c5b1ccc4084119e062902ab31612973477e5ac7
Jonathan Nicholas <jnic14@student.monash.edu>
2017-01-29 18:23:05 +1100

    Removing authkeys from master
    It's secret, senpai

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from twitter import *
 from nltk.corpus import words
 import time
 import random
-auth = OAuth(consumer_key='z8ZmHH8kChc9XmP9OYI1rL9pE',consumer_secret='ih0tsDeJVmJtnWVL4eDoFfvBzposjX6Y1zhNBUPh7GQSYykeaW',token='825594302390628352-ohzd6I2x0G7l66BC369e7Iw9zfOY472',token_secret='79Zuu6B6P38tjtAtm0fzzVdkat6QkxdCYnEj7oSxKbpu0')
 t = Twitter(auth=auth)
 wordlist = words.words()
 wordLength = len(wordlist)
